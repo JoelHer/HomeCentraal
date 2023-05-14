6bb2b87093e5d3a47f16e87b90d158009f547e57
iHouqLF <73959649+JoelHer@users.noreply.github.com>
2023-05-14 17:36:41 +0200

    stuff

--- src/renderer/components/WelcomePage.tsx
+++ src/renderer/components/WelcomePage.tsx
@@ -9,22 +9,19 @@ export default function WelcomePage() {
         <div className="Hello">
           <h5 className="desc-text text-center">All your smarthome devices in one place.</h5>
         </div>
+        <h6 className="desc-text text-center">To continue, please choose a mode of connection</h6>
         <br></br>
-        <h6 className="desc-text text-center">Choose mode of connection</h6>
         <div className="container text-center">
             <div className="row">
                 <div className="col align-self-start">
                     <h5 className="desc-text">Connect to Server</h5>
+                    <p>Let Homecentraal connect to a server on your network</p>
+                    <button className="btn btn-outline-success">Connect to server</button>
                 </div>
                 <div className="col align-self-center">
                     <h5 className="desc-text">Run in Local</h5>
-                    <div data-bs-theme="dark" className=".welcome-choose card text-center mb-3" style={{width: "18rem"}}>
-                        <div className="card-body">
-                            <h5 className="card-title">Special title treatment</h5>
-                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
+                    <p>Let Homecentraal run only on your computer.</p>
+                    <button className="btn btn-outline-primary">Run locally</button>
                 </div>
             </div>
         </div>
