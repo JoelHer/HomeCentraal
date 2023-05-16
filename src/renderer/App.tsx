82425292d3beb8d413db49da1b7d285c284ba8e0
iHouqLF <73959649+JoelHer@users.noreply.github.com>
2023-05-16 22:57:26 +0200

    kp

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,13 +1,72 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import './App.css';
 
-import WelcomePage from './components/WelcomePage';
+
+var foundDevices = "false";
+function Home() {
+  const [isShown, setIsShown] = useState(true);
+  setTimeout(() => {
+    foundDevices = "true";
+  }, 3000);
+  if (isShown) {
+    return (
+      <div>
+        <h1 className='text-center title-text'>Installation</h1>
+        <div className="Hello">
+          <h5 className="desc-text text-center">We are looking for smarthome devices on your local network, please stand by...</h5>
+          {foundDevices}
+        </div>
+      </div>
+    ) 
+  }
+  return ( null )
+}
+
+function WelcomePage() {
+  const [isShown, setIsShown] = useState(true);
+  function onSelectEnv() {
+    setIsShown(false);
+  }
+
+  if (isShown) {
+    return (
+      <div>
+        <h1 className='text-center title-text'>Homecentraal</h1>
+        <div className="Hello">
+          <h5 className="desc-text text-center">All your smarthome devices in one place.</h5>
+        </div>
+        <h6 className="desc-text text-center">To continue, please choose a mode of connection</h6>
+        <br></br>
+        <div className="container text-center">
+            <div className="row">
+                <div className="col align-self-start">
+                    <h5 className="desc-text">Connect to Server</h5>
+                    <p>Let Homecentraal connect to a server on your network</p>
+                    <button className="btn btn-outline-success" disabled>Connect to server</button>
+                </div>
+                <div className="col align-self-center">
+                    <h5 className="desc-text">Run in Local</h5>
+                    <p>Let Homecentraal run only on your computer.</p>
+                    <button className="btn btn-outline-primary" onClick={onSelectEnv}>Run locally</button>
+                </div>
+            </div>
+        </div>
+      </div>
+    ) 
+  }
+  return (
+    <Home />
+  )
+}
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<WelcomePage />} />
+        <Route path="/" element={
+          <WelcomePage />
+        }/>
       </Routes>
     </Router>
   );
--- src/renderer/components/WelcomePage.tsx
+++ src/renderer/components/WelcomePage.tsx
@@ -2,29 +2,32 @@ import React from "react"
 
 
 
-export default function WelcomePage() {
-    return (
-      <div>
-        <h1 className='text-center title-text'>Homecentraal</h1>
-        <div className="Hello">
-          <h5 className="desc-text text-center">All your smarthome devices in one place.</h5>
-        </div>
-        <h6 className="desc-text text-center">To continue, please choose a mode of connection</h6>
-        <br></br>
-        <div className="container text-center">
-            <div className="row">
-                <div className="col align-self-start">
-                    <h5 className="desc-text">Connect to Server</h5>
-                    <p>Let Homecentraal connect to a server on your network</p>
-                    <button className="btn btn-outline-success">Connect to server</button>
-                </div>
-                <div className="col align-self-center">
-                    <h5 className="desc-text">Run in Local</h5>
-                    <p>Let Homecentraal run only on your computer.</p>
-                    <button className="btn btn-outline-primary">Run locally</button>
-                </div>
-            </div>
-        </div>
+export default function WelcomePage( ) {
+  function handleClick() {
+    console.log('clicked');
+  }
+  return (
+    <div>
+      <h1 className='text-center title-text'>Homecentraal</h1>
+      <div className="Hello">
+        <h5 className="desc-text text-center">All your smarthome devices in one place.</h5>
       </div>
-    )
+      <h6 className="desc-text text-center">To continue, please choose a mode of connection</h6>
+      <br></br>
+      <div className="container text-center">
+          <div className="row">
+              <div className="col align-self-start">
+                  <h5 className="desc-text">Connect to Server</h5>
+                  <p>Let Homecentraal connect to a server on your network</p>
+                  <button className="btn btn-outline-success" disabled>Connect to server</button>
+              </div>
+              <div className="col align-self-center">
+                  <h5 className="desc-text">Run in Local</h5>
+                  <p>Let Homecentraal run only on your computer.</p>
+                  <button className="btn btn-outline-primary" onClick={handleClick}>Run locally</button>
+              </div>
+          </div>
+      </div>
+    </div>
+  )
 }
